e02347113f460789d56bb6dbcb9e470e77d55352
nyia <nyiaxnoel@gmail.com>
2022-11-21 15:00:14 -0500

    nycommit

--- tiktok.py
+++ tiktok.py
@@ -1,3 +1,4 @@
+# List who you have worked with on this project: Nyia George, Isabel Lopez, Caria Blevins 
 import json
 import unittest
 import os
@@ -7,11 +8,11 @@ from xml.sax import parseString
 from bs4 import BeautifulSoup
 
 API_KEY = "5b17fb869d9cf530dd9a6b45cf9228bf"
-HEADERS = {'Authorization': 'Bearer {}'.format(API_KEY),
-           'User-Agent': 'YelpEventAnalyzer',
-           'From': 'himharis@icloud.com',
-           'Course-Info': 'https://si.umich.edu/programs/courses/507'
-}
+# HEADERS = {'Authorization': 'Bearer {}'.format(API_KEY),
+#            'User-Agent': 'YelpEventAnalyzer',
+#            'From': 'himharis@icloud.com',
+#            'Course-Info': 'https://si.umich.edu/programs/courses/507'
+# }
 
 def read_json(cache_filename):
     try: 
@@ -25,7 +26,47 @@ def write_json(cache_filename, dict):
     with open(cache_filename, 'w') as file: 
         file.write(dump_file) 
 
-# def get_request_url(list):
-#     r = requests.get('https://open.tiktokapis.com/v2/user/info')
-#     #url = f"https://open.tiktokapis.com/v2/user/info/5b17fb869d9cf530dd9a6b45cf9228bf"
-#     HEADERS = {{'authorization': 'Bearer' + API_KEY = "5b17fb869d9cf530dd9a6b45cf9228bf"}}
+def get_request_url(r):
+    HEADERS = {'authorization': 'Bearer ' + API_KEY}
+    r = requests.get('https://open.tiktokapis.com/v2/user/info', headers = HEADERS)
+    #url = f"https://open.tiktokapis.com/v2/user/info/5b17fb869d9cf530dd9a6b45cf9228bf"
+   # HEADERS = {'authorization': 'Bearer ' + API_KEY}
+    print(r.text)
+
+def get_data_using_cache(list, cache_filename):
+    request_url =  get_request_url(list)
+    dictionary = read_json(cache_filename)
+    if request_url in dictionary:
+        print("Using cache for {}".format(list))
+        return dictionary[request_url]
+    else: 
+        print("Fetching data for {}".format(list))
+        try: 
+            url = requests.get(request_url).text
+            book_info = json.loads(url)
+            if book_info["status"] == "OK":
+                dictionary[request_url] = book_info["results"]
+                write_json(cache_filename, dictionary)
+                return dictionary.get(request_url)
+            else: 
+                print("No list found for list name provided.")
+                return None 
+        except:
+            print("Exception")
+            return None
+
+def display_name():
+    #displays the username 
+    pass
+def follower_count(): 
+    #displays the user's followers count 
+    pass
+def likes_count(): 
+    #shares the total number of likes received by the user across all of their videos 
+    pass 
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+    unittest.main(verbosity = 2)
