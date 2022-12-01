a1fb16a2eb228ce5e2a3182d403a87c8f4c1d214
nyigeo <51497678+nyigeo@users.noreply.github.com>
2022-12-01 09:21:35 -0500

    Update wikifruit.py

--- wikifruit.py
+++ wikifruit.py
@@ -7,17 +7,37 @@ from xml.sax import parseString
 from bs4 import BeautifulSoup
 
 
-url = 'https://simple.wikipedia.org/wiki/List_of_fruits'
-response = requests.get(url)
-soup = BeautifulSoup(response.content,'html.parser')
-#var = 'component page-object text'
-pay = soup.find_all('ul', class_= "mw-body-content mw-content-ltr")
-#listofpay = []
-#print(soup)
-# for tag in pay:
-#     print(tag.text)
-# something = tag.find_all('p')
-# if len(something) != 0:
-#listofpay.append(something[0].text)
-for data in pay: 
-   print(data)
+def get_request_url():
+    url = 'https://simple.wikipedia.org/wiki/List_of_fruits'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content,'html.parser')
+
+    pay = soup.find_all('li')
+    for li in pay:
+        content = li.find('a')
+        for item in content:
+            if str(item).isalpha():
+                print(item)
+
+# url = 'https://simple.wikipedia.org/wiki/List_of_fruits'
+# response = requests.get(url)
+# soup = BeautifulSoup(response.content,'html.parser')
+# #var = 'component page-object text'
+# pay = soup.find_all('ul', class_= "mw-body-content mw-content-ltr")
+# #listofpay = []
+# #print(soup)
+# # for tag in pay:
+# #     print(tag.text)
+# # something = tag.find_all('p')
+# # if len(something) != 0:
+# #listofpay.append(something[0].text)
+# for data in pay: 
+#    print(data)
+
+
+def main():
+    get_request_url()
+
+if __name__ == "__main__":
+    main()
+    unittest.main(verbosity = 2)
