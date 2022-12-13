7ef816816b19acab7be53ac059c76ceb1c2039df
nyigeo <51497678+nyigeo@users.noreply.github.com>
2022-12-12 23:55:55 -0500

    Update wikifruit.py

--- wikifruit.py
+++ wikifruit.py
@@ -6,23 +6,20 @@ import re
 import sqlite3
 from xml.sax import parseString
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
 
 def found_fruits():
-   url = 'https://snaped.fns.usda.gov/seasonal-produce-guide'
-   response = requests.get(url)
-   soup = BeautifulSoup(response.content,'html.parser')
-   #var = 'component page-object text'
-   fruits = soup.find('div',{'class':'paragraph paragraph--type-view paragraph--view-mode-default'})
-
-   season_list = ['Spring', 'Summer', 'Winter', 'Fall','&']
-   
-   fruit_list = []
-   for fruit in fruits.text.split():
+    url = 'https://snaped.fns.usda.gov/seasonal-produce-guide'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content,'html.parser')
+    fruits = soup.find('div',{'class':'paragraph paragraph--type-view paragraph--view-mode-default'})
+    season_list = ['Spring', 'Summer', 'Winter', 'Fall','&']
+    fruit_list = []
+    for fruit in fruits.text.split():
         if fruit not in season_list:
-            fruit_list.append(fruit)
-   return fruit_list
-#    letter_fruit = []
-#    for fruit in fruit_list:
+         fruit_list.append(fruit)
+    print(fruit_list)
+    return fruit_list 
 
 def pie_chart(fruit_list): 
    Bananas = fruit_list.count('Bananas')
@@ -34,25 +31,44 @@ def pie_chart(fruit_list):
    ax.pie(x, radius=3, center=(4, 4), labels = ['Bananas','Limes', 'Mushrooms', 'Onions'],shadow=True,startangle=90)
    ax.axis('equal')
    plt.tight_layout()
-   plt.show()  
-    
+   plt.show()
+
 def found_season():
-#    url = 'https://snaped.fns.usda.gov/seasonal-produce-guide'
-#    response = requests.get(url)
-#    soup = BeautifulSoup(response.content,'html.parser')
-#    response = requests.get(url)
-#    soup = BeautifulSoup(response.content,'html.parser')
- seasons_list = ['Spring', 'Summer', 'Winter', 'Fall']
-#    seasons = soup.find('div',{'class':'paragraph paragraph--type-view paragraph--view-mode-default'}).find('a').text
-# #    season_text = list(seasons.descendants)
-#    seasons_list.append(seasons)
-    # for season in seasons.text.split():
-    #      if 'Spring' 'Summer' 'Winter' 'Fall':
- return seasons_list 
+    seasons_list = ['Spring', 'Summer', 'Winter', 'Fall']
+    return seasons_list 
+
+def found_fruit_season():
+    url = 'https://snaped.fns.usda.gov/seasonal-produce-guide'
+    response = requests.get(url)
+    spring_list = []
+    summer_list = []
+    fall_list = []
+    winter_list = []
+    soup = BeautifulSoup(response.content,'html.parser')
+    spring_fruit = soup.find('div',{'class':'seasonal-produce--landing-season spring'}).find_all('a')
+
+    for fruit in spring_fruit[1:]:
+        spring_list.append(fruit.text)
+        print('Spring: ', spring_list)
+    fall_fruit = soup.find('div',{'class':'seasonal-produce--landing-season fall'}).find_all('a')
+    for fruit in fall_fruit[1:]:
+        fall_list.append(fruit.text)
+    print('Fall: ', fall_list)
+
+    summer_fruit = soup.find('div',{'class':'seasonal-produce--landing-season summer'}).find_all('a')
+    for fruit in summer_fruit[1:]:
+        summer_list.append(fruit.text)
+    print('Summer: ', summer_list)
+    winter_fruit = soup.find('div',{'class':'seasonal-produce--landing-season winter'}).find_all('a')
+    for fruit in winter_fruit[1:]:
+        winter_list.append(fruit.text)
+    print('Winter: ', winter_list)
+    return ('Spring: ', spring_list), ('Summer: ', summer_list),('Fall: ', fall_list),('Winter: ', winter_list)
 
 def found_temperature(): 
-   temperature_list = ['Celsius', 'Fahrenheit']
-   return temperature_list
+    temperature_list = ['Celsius', 'Fahrenheit']
+    return temperature_list
+
 
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -60,7 +76,6 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
-# Creates list of species ID's and numbers
 def create_fruits_table(cur, conn, fruit_list):
 
     cur.execute("DROP TABLE IF EXISTS matchedfruit")
@@ -68,12 +83,17 @@ def create_fruits_table(cur, conn, fruit_list):
     for i in range(len(fruit_list)):
         cur.execute("INSERT INTO matchedfruit (id,FruitName) VALUES (?,?)",(i,fruit_list[i]))
     conn.commit()
+
 def create_seasons_table(cur, conn, seasons_list):
 
     cur.execute("DROP TABLE IF EXISTS matchedseasons")
     cur.execute("CREATE TABLE matchedseasons (id INTEGER PRIMARY KEY, SeasonName TEXT)")
-    for i in range(len(seasons_list)):
-        cur.execute("INSERT INTO matchedseasons (id,SeasonName) VALUES (?,?)",(i,seasons_list[i]))
+    fruit_count = cur.fetchone()[0]
+    count = 1
+    id = count 
+    for i in range(fruit_count,count + 25):
+        cur.execute("INSERT INTO matchedseasons (id,SeasonName) VALUES (?,?)",(i,seasons_list[i])).fetchone()[0]
+        id += 1
     conn.commit()
 
 def create_temperature_table(cur, conn, temperature_list):
@@ -85,24 +105,17 @@ def create_temperature_table(cur, conn, temperature_list):
     conn.commit()
 
 def main():
-    # SETUP DATABASE AND TABLE
     something = found_fruits()
     something2 = found_season()
     something3 = found_temperature()
     cur, conn = setUpDatabase('fruits.db')
+    found_fruit_season()
     create_fruits_table(cur, conn, something)
     create_seasons_table(cur, conn, something2)
     create_temperature_table(cur, conn, something3)
-   
+    found_season()
     pie_chart(something)
-    # create_patients_table(cur, conn)
-   #  add_fluffle(cur, conn)
-   #  add_pets_from_json('pets.json', cur, conn)
-   #  ls = (non_aggressive_pets(10, cur, conn))
-   #  print(ls)
 
-    
-    
 if __name__ == "__main__":
-     main()
-     unittest.main(verbosity = 2)
+    main()
+    unittest.main(verbosity = 2)
